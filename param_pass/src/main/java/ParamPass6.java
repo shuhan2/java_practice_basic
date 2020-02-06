a2d330fa5777eb162ddc0ff335b881298e4661e6
nonpool <nonpool@163.com>
2020-02-06 15:15:11 +0800

    修改描述

--- param_pass/src/main/java/ParamPass6.java
+++ param_pass/src/main/java/ParamPass6.java
@@ -5,13 +5,13 @@ public class ParamPass6 {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2};
 
-        System.out.println("交换前 a:" + Arrays.toString(a));
-        swap(a);
-        System.out.println("交换后 a:" + Arrays.toString(a));
+        System.out.println("改变前 a:" + Arrays.toString(a));
+        change(a);
+        System.out.println("改变后 a:" + Arrays.toString(a));
     }
 
 
-    public static void swap(int[] a) {
+    public static void change(int[] a) {
         a = new int[]{3, 4};
 
         System.out.println("方法中 a:" + Arrays.toString(a));
